74c678155cc5d9763c11eaa10a99cf8e3cbd4420
Korben Kirscht <kirscht@gmail.com>
2020-11-19 20:28:44 -0800

    Initial setup.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 curl -o requirements.yml https://raw.githubusercontent.com/galaxy3-net/vagrant_setup/setup/requirements.yml
-ansible-galaxy install -r requirements.yml
+ansible-galaxy role install -r requirements.yml
 
 # ansible-playbook -i 127.0.0.1, -K ~/.ansible/roles/kirscht.week_10_day_2/setup.yml
